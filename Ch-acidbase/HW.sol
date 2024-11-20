8fd9fd94cd0c866c110011badc324bbdd03939fa
TorresAWS <d.torres.rangel@gmail.com>
2024-11-20 13:22:18 -0500

    added new branch Ch-acidbase to master

---
+++ Ch-acidbase/HW.sol
@@ -0,0 +1,50 @@
+\eqTopOfSolnPage
+\eqTopOfQslPage
+\withinsoldoctrue
+\setSolnMargins{18.5278pt}
+\eqgriii\noindent\begin{eqequestions}
+\ReturnTo{qex.1}{\mbox{$\square $}}\endeqEXt\par{{}}%
+
+{1in}{     }
+\ReturnTo{qex.1}{\mbox{$\square $}}\endeqEXt\par{{}}%
+
+\ReturnTo{qex.2}{\mbox{$\square $}}\endeqEXt\par{{}}%
+
+{1in}{     }
+\ReturnTo{qex.2}{\mbox{$\square $}}\endeqEXt\par{{}}%
+
+\ReturnTo{qex.3}{\mbox{$\square $}}\endeqEXt\par{{}}%
+
+{1in}{     }
+\ReturnTo{qex.3}{\mbox{$\square $}}\endeqEXt\par{{}}%
+
+\ReturnTo{qex.4}{\mbox{$\square $}}\endeqEXt\par{{}}%
+
+{1in}{     }
+\ReturnTo{qex.4}{\mbox{$\square $}}\endeqEXt\par{{}}%
+
+\ReturnTo{qex.5}{\mbox{$\square $}}\endeqEXt\par{{}}%
+
+{1in}{     }
+\ReturnTo{qex.5}{\mbox{$\square $}}\endeqEXt\par{{}}%
+
+\ReturnTo{qex.6}{\mbox{$\square $}}\endeqEXt\par{{}}%
+
+{1in}{     }
+\ReturnTo{qex.6}{\mbox{$\square $}}\endeqEXt\par{{}}%
+
+\ReturnTo{qex.7}{\mbox{$\square $}}\endeqEXt\par{{}}%
+
+{1in}{     }
+\ReturnTo{qex.7}{\mbox{$\square $}}\endeqEXt\par{{}}%
+
+\ReturnTo{qex.8}{\mbox{$\square $}}\endeqEXt\par{{}}%
+
+{1in}{     }
+\ReturnTo{qex.8}{\mbox{$\square $}}\endeqEXt\par{{}}%
+
+\eqgrii\end{eqequestions}
+
+\btwnExamSkip
+
+\endinput
